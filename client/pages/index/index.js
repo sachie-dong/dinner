1d171f14fa94afe416ff8afc3b37398427b2ed9b
songhaiyi <15135094322@163.com>
2018-05-21 09:13:18 +0800

    修改主页

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -6,9 +6,51 @@ const app = getApp()
 Page({
   data: {
     selected: true,
-    selected1: false
+    selected1: false,
+    // 切换
+    category: [
+      { name: '果味', id: 'guowei' },
+      { name: '凉菜', id: 'liang' },
+      { name: '热菜', id: 're' },
+      { name: '其他', id: 'else' }
+    ],
+    detail: [],
+    curIndex: 0,
+    isScroll: false,
+    toView: 'guowei'
   },
   //事件处理函数
+  // 切换
+  onReady() {
+    var self = this;
+    // wx.request({
+    //     url:'http://www.gdfengshuo.com/api/wx/cate-detail.txt',
+    //     success(res){
+    //         self.setData({
+    //             detail : res.data
+    //         })
+    //     }
+    // });
+
+  },
+  switchTab(e) {
+    const self = this;
+    this.setData({
+      isScroll: true
+    })
+    setTimeout(function () {
+      self.setData({
+        toView: e.target.dataset.id,
+        curIndex: e.target.dataset.index
+      })
+    }, 0)
+    setTimeout(function () {
+      self.setData({
+        isScroll: false
+      })
+    }, 1)
+
+  },
   //tab
   selected: function (e) {
     this.setData({
